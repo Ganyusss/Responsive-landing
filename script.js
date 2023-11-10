4ff4c84cd5c461f58c8163eff8cea623261b100b
Ganyusss <148171735+Ganyusss@users.noreply.github.com>
2023-11-10 11:14:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showDetails() {
+    document.getElementById('detailsSection').style.display = 'block';
+}
+
+function hideDetails() {
+    document.getElementById('detailsSection').style.display = 'none';
+}
